e4739d09d7629e5d8f0dd79a1f412a824e976ed5
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-03-03 19:07:12 -0300

    fix: remove unused tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Ponderada-SPA-Angular' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Ponderada-SPA-Angular');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Ponderada-SPA-Angular');
+    expect(compiled.querySelector('h2')?.textContent).toContain('Job Application for Heroes');
   });
 });
